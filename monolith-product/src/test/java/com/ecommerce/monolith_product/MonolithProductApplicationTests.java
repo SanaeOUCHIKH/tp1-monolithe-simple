9ee77cf5b6949f469abad98d9194fb23fde8bc6f
OUCHIKH Sana <ouchikhsanae11@gmail.com>
2026-04-05 08:47:46 +0200

    tp1 sans exercice

---
+++ monolith-product/src/test/java/com/ecommerce/monolith_product/MonolithProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.monolith_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
